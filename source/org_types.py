75d25f96375ee12e9b48f7275c7650c66a30c52c
Spencer Bartz <sbartz@invoca.com>
2016-02-09 13:32:19 -0800

    investigate org_types.py

---
+++ source/org_types.py
@@ -0,0 +1,18 @@
+ORG_TYPE_DOCNAMES = [
+  'network_user',
+  'advertiser_user',
+  'affiliate_user'
+]
+
+ORG_TYPES = {
+  '@@ORG_TYPE_PLURAL': [
+    'networks',
+	'advertisers',
+	'affiliates'
+  ],
+  '@@ORG_TYPE_SINGULAR': [
+    'network',
+	'advertiser',
+	'affiliate'
+  ]
+}
