edabc956b245d0a79dee43c315bd16f60dcdf5cc
Christopher Garoutte <cgaroutte@invoca.com>
2022-09-28 15:23:22 -0700

    STORY-12292: simplify disabling Try It Out section

--- source/_static/js/swagger-ui/swagger-initializer.js
+++ source/_static/js/swagger-ui/swagger-initializer.js
@@ -1,31 +1,19 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-  // https://github.com/swagger-api/swagger-ui/issues/3725#issuecomment-334899276
-  const DisableTryItOutPlugin = function() {
-    return {
-      statePlugins: {
-        spec: {
-          wrapSelectors: {
-            allowTryItOutFor: () => () => false
-          }
-        }
-      }
-    }
-  }
-
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "https://petstore.swagger.io/v2/swagger.json", // TODO: replace with real schema when versioning is in-place
     dom_id: '#swagger-ui',
-    deepLinking: "True",
+    deepLinking: true,
     syntaxHighlight: { theme: 'arta'},
     presets: [
       SwaggerUIBundle.presets.apis,
     ],
     plugins: [
-      DisableTryItOutPlugin
-    ]
+    ],
+    tryItOutEnabled: false,
+    supportedSubmitMethods: ['']
   });
 
   //</editor-fold>
